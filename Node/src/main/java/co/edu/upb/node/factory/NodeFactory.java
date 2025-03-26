ec4b229dbe15b0b99e229d26171dc5c9b56b78b4
David Cárdenas <santito500@live.com>
2025-03-26 14:58:05 -0500

    refactor: Implementation of RMI client
    Now the RMI client is not a class from infrastructure, but it's added through the factory by looking at the registry.

--- Node/src/main/java/co/edu/upb/node/factory/NodeFactory.java
+++ Node/src/main/java/co/edu/upb/node/factory/NodeFactory.java
@@ -2,10 +2,16 @@ package co.edu.upb.node.factory;
 
 import co.edu.upb.node.application.ConversionManager;
 import co.edu.upb.node.application.WorkersManager;
-import co.edu.upb.node.infrastructure.NodeRegistry;
+import co.edu.upb.node.config.Environment;
+import co.edu.upb.node.domain.interfaces.infrastructure.InterfacePublisher;
 import co.edu.upb.node.infrastructure.NodeServer;
 import co.edu.upb.node.util.ChromeExecutor;
 import co.edu.upb.node.util.OfficeExecutor;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 
 public class NodeFactory {
 
@@ -17,9 +23,15 @@ public class NodeFactory {
 
         ConversionManager conversionManager = new ConversionManager(officeExecutor, chromeExecutor);
 
-        NodeRegistry nodeRegistry = new NodeRegistry();
+        //Look for the implementation of the RMI registry of the Auth Server.
+        InterfacePublisher registry;
+        try {
+            registry = (InterfacePublisher) Naming.lookup("rmi://" + Environment.getInstance().getDotenv().get("AUTH_URL"));
+        } catch (NotBoundException | MalformedURLException | RemoteException e) {
+            throw new RuntimeException(e);
+        }
 
-        return new NodeServer(conversionManager, nodeRegistry);
+        return new NodeServer(conversionManager, registry);
     }
 
 }
